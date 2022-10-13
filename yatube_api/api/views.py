44cd3c7f2cbaee2ecada52f5bd52b15836af90e6
Yury Zharikov <zharikovyuri@gmail.com>
2022-10-13 16:47:38 +0300

    11/28 tests

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,4 +1,5 @@
 from .serializers import PostSerializer, CommentSerializer
+from .pagination import PostsPaginator
 from posts.models import Post, Comment
 from rest_framework import viewsets
 from rest_framework.response import Response
@@ -9,6 +10,7 @@ from rest_framework.response import Response
 class PostsViewSet(viewsets.ModelViewSet):
     serializer_class = PostSerializer
     queryset = Post.objects.all()
+    pagination_class = PostsPaginator
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
